70fb9d7e9a5866f3148ae1eae76676380fbb0f4f
Majed Al-Chatti <alchatti@gmail.com>
2021-10-27 12:55:07 +0400

    WIP with DB

--- scripts/init-d9-acquia.sh
+++ scripts/init-d9-acquia.sh
@@ -1,5 +1,7 @@
 cd /var/www/html
+rm .keep
 composer create-project --no-install drupal/recommended-project:^9.0.0 .
 sed -i'.original' 's/web\//docroot\//g' composer.json && rm composer.json.original
-composer require drush/drush:^10.2 drupal/mysql56 --no-update
+# composer require drush/drush:^10.2 drupal/mysql56 --no-update
+composer require drush/drush:^10.2
 composer update
