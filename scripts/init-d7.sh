5f7161d86f8299266138895ec209fee76df5700f
Majed Al-Chatti <alchatti@gmail.com>
2021-11-08 15:44:36 +0400

    init scripts

---
+++ scripts/init-d7.sh
@@ -0,0 +1,2 @@
+drush dl drupal-7 --destination=/var/www/html/ --y
+mv drupal-7.* $APACHE_DOCUMENT_ROOT
---
+++ scripts/init-d8.sh
@@ -0,0 +1,2 @@
+drush dl drupal-8 --destination=/var/www/html/ --y
+mv drupal-8.* $APACHE_DOCUMENT_ROOT
