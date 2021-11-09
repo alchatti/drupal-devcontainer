4c0466669e5c658610c3d6353030b214f3c548ab
Majed Al-Chatti <alchatti@gmail.com>
2021-11-09 16:44:49 +0400

    local settings

---
+++ config/settings.local.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Local development override configuration feature.
+ *
+ * To activate this feature, copy content of 'sites/default/example.settings.local.php'.
+ * Then, go to the bottom of * 'sites/default/settings.php' and uncomment the commented
+ * lines that mention * 'settings.local.php'.
+ */
