fb8a6ef5618208ebf1c1a87a3cffe852912838f1
Majed Al-Chatti <alchatti@gmail.com>
2022-04-12 09:33:00 +0400

    Moved to Prebuilt image

---
+++ .devcontainer/build.sh
@@ -0,0 +1,6 @@
+docker build \
+--build-arg VARIANT=8.1-apache-bullseye \
+--build-arg NODE_VERSION=17 \
+--build-arg DART_SASS_VERSION=1.50.0 \
+--build-arg VARIANT=8.1-apache-bullseye \
+-t alchatti/drupal-devcontainer:1 .
