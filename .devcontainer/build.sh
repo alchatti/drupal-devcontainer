c5f88be864df669d506a514d80ed623748ad1480
Majed Al-Chatti <alchatti@gmail.com>
2022-04-13 12:57:45 +0400

    Update Docker Image config

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+PHP=8.1
+NODE=17
+SASS=1.50.0
+
 docker build \
---build-arg VARIANT=8.1-apache-bullseye \
---build-arg NODE_VERSION=17 \
---build-arg DART_SASS_VERSION=1.50.0 \
--t alchatti/drupal-devcontainer:1 .
+--build-arg VARIANT=$PHP-apache-bullseye \
+--build-arg NODE_VERSION=$NODE \
+--build-arg DART_SASS_VERSION=$SASS \
+-t alchatti/drupal-devcontainer:$PHP-17-1.50.0 -t alchatti/drupal-devcontainer:latest .
