9ff58fc0b12af90b45aeb3095c20af572ecc18a4
Majed Al-Chatti <alchatti@gmail.com>
2022-04-14 14:47:49 +0400

    clean up

--- scripts/init-d9-acquia.sh
+++
@@ -1,7 +0,0 @@
-cd $WORKSPACE_ROOT
-# Source https://docs.acquia.com/cloud-platform/create/install/drupal9/
-composer create-project --no-install drupal/recommended-project:^9.0.0 .
-sed -i'.original' 's/web\//docroot\//g' composer.json && rm composer.json.original
-# composer require drush/drush:^10.2 drupal/mysql56 --no-update
-composer require drush/drush:^10.2
-composer update
