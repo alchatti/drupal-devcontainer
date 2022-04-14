c5913123e2345764bbab36ee95046705db605fef
Majed Al-Chatti <alchatti@gmail.com>
2022-04-14 14:46:51 +0400

    Powershell profile

--- .devcontainer/.build/powershell_profile.ps1
+++ .devcontainer/.build/powershell_profile.ps1
@@ -1,2 +1,6 @@
 oh-my-posh init pwsh --config /opt/.poshthemes/$env:POSH_THEME_ENVIRONMENT.omp.json | Invoke-Expression
-Import-Module "posh-git", "posh-sshell" ,"Terminal-Icons"
+Import-Module "posh-git","PSReadLine","Terminal-Icons","posh-sshell"
+
+# PSReadLine
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
