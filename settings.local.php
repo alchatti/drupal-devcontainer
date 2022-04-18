725e73a7a17f3398ac915a9daab0a49599a25004
Majed Al-Chatti <alchatti@gmail.com>
2022-04-18 09:29:22 +0400

    Switch to env file and added local settings

--- settings.local.php
+++ settings.local.php
@@ -4,7 +4,18 @@
  * @file
  * Local development override configuration feature.
  *
- * To activate this feature, copy content of 'sites/default/example.settings.local.php'.
- * Then, go to the bottom of * 'sites/default/settings.php' and uncomment the commented
- * lines that mention * 'settings.local.php'.
+ * To activate this file, copy the following code to the end of your settings.php
+ ** if (file_exists('/var/www/settings.local.php')) { require('/var/www/settings.local.php'); }
  */
+$base_url = 'http://localhost';
+$options['uri'] = 'http://localhost';
+$databases['default']['default'] = array(
+  'database' => $_ENV['MYSQL_DATABASE'],
+  'username' => $_ENV['MYSQL_USER'],
+  'password' => $_ENV['MYSQL_PASSWORD'],
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
