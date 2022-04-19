3dfac5eace22b198febf9a1897948abfbc54e188
Majed Al-Chatti <alchatti@gmail.com>
2022-04-19 13:44:24 +0400

    PNPM store mount & multi-build

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
+
 print_usage() {
-  printf "\nUsage: -p phpVer -s sassVer -n nodeJSVer\n\nIf no version is provided the latest will be used\n\n"
+  printf "\nUsage: -p phpVer -s sassVer -n nodeJSVer\n\nIf no version is provided the latest will be used\n\n# For list of tags vsist https://mcr.microsoft.com/v2/vscode/devcontainers/php/tags/list\n\n"
 }
 
 while getopts p:s:n: flag;
@@ -13,14 +16,24 @@ do
     esac
 done
 
-if [ -z "$PHP" ]; then PHP=8.1-apache-bullseye; echo "No version setting PHP to default > $PHP";fi
+if [ -z "$PHP" ]
+then
+  PHP=("8.1-apache-bullseye" "7.4-apache-bullseye")
+  echo "No version setting PHP to defaults"
+else
+  PHP=($PHP);
+fi;
+
 if [ -z "$NODE" ]; then NODE=$(curl -s https://api.github.com/repos/nodejs/node/releases/latest | grep "tag_name.*" | cut -d ":" -f 2 | cut -d "\"" -f 2 | sed 's/v//g'); echo "No version provided setting NODE to latest > $NODE";fi
 if [ -z "$SASS" ]; then SASS=$(curl -s https://api.github.com/repos/sass/dart-sass/releases/latest | grep "tag_name.*" | cut -d ":" -f 2 | cut -d "\"" -f 2); echo "No version provided setting SASS to latest > $SASS";fi
 
-printf "\nBuilding image with \n - PHP $PHP \n - NodeJs $NODE \n - dart-sass $SASS \n\n"
 
-docker build \
---build-arg VARIANT=$PHP \
---build-arg NODE_VERSION=$NODE \
---build-arg DART_SASS_VERSION=$SASS \
--t alchatti/drupal-devcontainer:p$PHP-n$NODE-s$SASS -t alchatti/drupal-devcontainer:latest .
+for phpver in ${PHP[@]}
+do
+  printf "\nBuilding image with \n - PHP $phpver \n - NodeJs $NODE \n - dart-sass $SASS \n\n"
+  docker build \
+  --build-arg VARIANT=$phpver \
+  --build-arg NODE_VERSION=$NODE \
+  --build-arg DART_SASS_VERSION=$SASS \
+  -t alchatti/drupal-devcontainer:p$phpver-n$NODE-s$SASS -t alchatti/drupal-devcontainer:$phpver-latest .
+done
