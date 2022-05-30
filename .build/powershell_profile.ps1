de2d94d25c447f1c73910f5a902d7c5ed0581b95
Majed Al-Chatti <alchatti@gmail.com>
2022-05-30 11:48:23 +0000

    refactor: Moved image build to another repo
    https://github.com/alchatti/drupal-devcontainer-image

--- .build/powershell_profile.ps1
+++
@@ -1,7 +0,0 @@
-oh-my-posh init pwsh --config /opt/.poshthemes/$env:POSH_THEME_ENVIRONMENT.omp.json | Invoke-Expression
-Import-Module "posh-git","PSReadLine","Terminal-Icons","posh-sshell"
-
-# PSReadLine
-Set-PSReadLineOption -EditMode Windows
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
